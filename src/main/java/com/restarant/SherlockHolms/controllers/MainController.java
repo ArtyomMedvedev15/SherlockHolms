9696b80d89739e616bfe75217c07953000993437
Tim <arthaos14@gmail.com>
2019-05-16 18:58:24 +0300

    Add main page without photo, and start work to fully page

--- src/main/java/com/restarant/SherlockHolms/controllers/MainController.java
+++ src/main/java/com/restarant/SherlockHolms/controllers/MainController.java
@@ -1,13 +1,27 @@
 package com.restarant.SherlockHolms.controllers;
 
+import com.restarant.SherlockHolms.repos.ChefsRepo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
+    private final ChefsRepo chefsRepo;
+
+    public MainController(ChefsRepo chefsRepo) {
+        this.chefsRepo = chefsRepo;
+    }
+
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("chefs",chefsRepo.findAll());
+
         return "greeting";
     }
+
+
+
+
 }
